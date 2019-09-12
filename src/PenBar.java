cf9571c7e51cfbfbbecc6e63c4f85f28d131bedf
raec5400 <raec5400@ISS2V1PKH2.scdsb.pri>
2019-09-12 11:59:14 -0400

    Updated 9/12/19

--- src/PenBar.java
+++ src/PenBar.java
@@ -1,4 +1,15 @@
+import hsa.Console;
+import java.awt.Color;
+
 
 public class PenBar {
+    int x, y, height;
+    Color col;
     
+    public void draw(Console c){
+        c.setColor(col);
+        y= 400-height;
+        c.fillRect(x, y, height, 50);
+        c.drawString("Value: " + height, x-10, y);
+}
 }
