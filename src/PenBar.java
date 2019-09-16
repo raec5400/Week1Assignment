5ee996db9573be6f9079b0327058ec0387c939f5
Rae Chang <Rae Chang@DESKTOP-BB5M2SI>
2019-09-16 17:29:17 -0400

    Update 9/16/2019

--- src/PenBar.java
+++ src/PenBar.java
@@ -3,13 +3,13 @@ import java.awt.Color;
 
 
 public class PenBar {
-    int x, y, height;
+    int x, y, width;
     Color col;
     
     public void draw(Console c){
         c.setColor(col);
-        y= 400-height;
-        c.fillRect(x, y, height, 50);
-        c.drawString("Value: " + height, x-10, y);
+        y= 400-width;
+        c.fillRect(x, y, width, 50);
+        c.drawString("Value: " + width, x+10, y);
 }
 }
